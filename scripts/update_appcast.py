9f0a98ca94ce7b00cb0aac0b09ecebfac552ca62
ytsteven <1647605516@qq.com>
2023-04-10 17:42:26 +0800

    iniit

--- scripts/update_appcast.py
+++ scripts/update_appcast.py
@@ -8,7 +8,7 @@ def update_appcast(message):
     with open ("src/info.json", "r") as f:
         info = json.load(f)
     version = info["version"]
-    release_file = Path(f"release/bob-plugin-akl-chatgpt-free-translate.bobplugin")
+    release_file = Path(f"release/easyuseai-plugin-akl-chatgpt-free-translate.aiplugin")
     assert release_file.is_file(), "Release file not exist"
     with open(release_file, "rb") as f:
         c = f.read()
@@ -17,15 +17,15 @@ def update_appcast(message):
         "version": version,
         "desc": message,
         "sha256": file_hash,
-        "url": f"https://github.com/akl7777777/bob-plugin-akl-chatgpt-free-translate/releases/download/v{version}/bob-plugin-akl-chatgpt-free-translate_v{version}.bobplugin",
-        "minBobVersion": "0.5.0"
+        "url": f"https://github.com/ytsteven/easyuseai-plugin-akl-chatgpt-free-translate/releases/download/v{version}/easyuseai-plugin-akl-chatgpt-free-translate_v{version}.aiplugin",
+        "minBobVersion": "0.4.3"
     }
     appcast_file = Path("appcast.json")
     if appcast_file.is_file():
         with open(appcast_file, "r") as f:
             appcast = json.load(f)
     else:
-        appcast = dict(identifier="com.akl.bob-plugin-akl-chatgpt-free-translate", versions=[])
+        appcast = dict(identifier="com.akl.easyuseai-plugin-akl-chatgpt-free-translate", versions=[])
     appcast["versions"].insert(0, version_info)
     with open(appcast_file, "w") as f:
         json.dump(appcast, f, ensure_ascii=False, indent=2)
